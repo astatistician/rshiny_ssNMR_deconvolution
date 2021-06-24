f86b3f07dc6fb989d43afc179885f065302bb3a5
Piotr <piotr.prostko@uhasselt.be>
2021-06-24 15:20:16 +0200

    bugfix: remove y- and x-axis limits on the plot.

--- R/auxiliary_functions.R
+++ R/auxiliary_functions.R
@@ -306,7 +306,7 @@ plot_model_fit <- function(model_fit) {
     add_trace(x = ~ model_fit$dat$ppm, y = ~ model_fit$dat$mix, name = "mixture spectrum", mode = "lines", line = list(color = "4682B4")) %>%
     add_trace(x = ~ model_fit$dat$ppm, y = ~ model_fit$dat$residuals, name = "residuals", mode = "lines", line = list(color = "red")) %>%
     add_trace(x = ~ model_fit$dat$ppm, y = ~ model_fit$dat$fitted, name = "fit", mode = "lines", line = list(color = "orange")) %>%
-    layout(xaxis = list(zeroline = FALSE, range = c(0, -50), title = "ppm"), yaxis = list(title = "intensity", range = c(0, 0.005))) %>%
+    layout(xaxis = list(zeroline = FALSE, title = "ppm"), yaxis = list(title = "intensity")) %>%
     event_register("plotly_legendclick")
   return(p)
 }
